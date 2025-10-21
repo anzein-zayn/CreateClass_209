405d20d76dae697a037ecb530bfd625b6fcd7845
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-10-21 11:18:47 +0700

    code PP

---
+++ PersegiPanjang.py
@@ -0,0 +1,23 @@
+class PersegiPanjang:
+    def __init__(self, panjang, lebar):
+        self.panjang =  panjang
+        self.lebar = lebar
+
+    def keliling(self):
+        return 2*(self.panjang + self.lebar)
+    
+    def luas(self):
+        return self.panjang * self.lebar
+    def __str__ (self):
+        return f"Persegi Panjang, Panjang {self.panjang} cm, Lebar {self.lebar} cm"
+    
+try:
+    panjang_input = int(input("Masukkan Panjang (cm) : "))
+    lebar_input = int(input("Masukkan Lebar (cm) : "))
+
+    pp = PersegiPanjang(panjang_input, lebar_input)
+    print("Keliling", pp.keliling(), "cm")
+    print("Luas", pp.luas(), "cm^2")
+
+except ValueError:
+        print("Input harus Berupa Angka!!!!!!!!!.")
